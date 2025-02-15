b1ea5519f5cb81725532d3537f8a173319ac89f3
jayson-creator <jaysongalimba@icloud.com>
2025-02-15 22:22:49 +0800

    Add files via upload

---
+++ JAYSON CODES.py
@@ -0,0 +1,34 @@
+INFORMATION = {
+    "INFRASTRUCTURE " : "The system of human made physical stuctures, for example roads, bridge, buildings, communication tower" ,
+    "INFRASTRUCUTRE DEVELOPMENT" : "The process of building and improving structures." ,
+    "Building Information Modeling (BMI)" : "A collaborative process for the creation, management and storage of information relating to a construcion project." ,
+    "Geotechnical Engineering" : "a branch of civil engineering that studies how earth materials behave in engineering systems." ,
+    "Structural Engineering" : "they are responsible for ensuring the structures are safe, strong, and stable." ,
+    "Environmental Engineering" : " work to protect the environment and human health by developing strategies and technologies." ,
+    "Transportation Engineering" : "they planned, design and manage of the facilities for any mode of transportation." ,
+    "Construction Enginerring" : "they build structures like houses, roads, and bridges." ,
+    "Water Management" : "the control and use of water drinking, farming, and industry." ,
+    "Roads" : "Path or streets for cars, buses and bikes to travel on." ,
+    "Railways" : "train tracks and station that healp people to move along distance" ,
+    "Bridge" : "A structure built over rivers, roads, or valleys to help people and vehicles cross." ,
+    "Tunnels" : "passage dug underground or through mountains for cars, trains." ,
+    "Dams" : "large walls built across rivers to store water and produce electricity." ,
+    "Drainage" : "pipes and channels that carry away rainwater to prevent flooding." ,
+    "Building" : " A structure where people live, work, and shop." ,
+    "Seaport" : "A place where ships load and unload goods and passengers." ,
+    "Airport" : "a place where airplanes take off, land, and carry people and package." ,
+    "Bus Terminal" : "A station where buses pick up and drop off thier passengers." ,
+    "Comercial Buildings" : "large buildings wtih shops, offices, and businesses." ,
+    "Telecommunication Tower" : " a tall structure that sends and receives phone and internet signals." ,
+    "Satellite Ground Station" : "a facility that communicates with satellites in space." ,
+    "Power Plants" : "factories that make electricity using fuel, water, wind, or sunlight." ,
+    "Solar Farms" : "large areas with solar panels that turn sunlight into electricity." ,
+    "Wind Farms" : "group of wind turbines that uses wind to make electricity." ,
+    "Landfills" : "places where trash and waste are buried." ,
+    "Power Grinds" : "system of wires and stations that bring electricity to homes and businesses." ,
+    "Water Distribution Pipes" : "pipes that carry water to homes and buildings." ,
+    "Irrigation System" : "pipes and canals that bring water to farms and crops."
+    }
+
+a = input ("Search")
+print (INFORMATION)
